6be0cf8d54b4b36db5edfc68694934a0cd58082c
sherggs <sherggs@gmail.com>
2026-07-20 00:24:31 +0100

    dynamodb: zero_trust_table

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "zt-table" {
+resource "aws_dynamodb_table" "zero_trust_table" {
   name         = "${var.project_name}-dynamodb-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "item_id"
