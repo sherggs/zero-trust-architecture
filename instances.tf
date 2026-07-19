b42e039f13dc192b2087280a651b0a8e2ee71774
sherggs <sherggs@gmail.com>
2026-07-19 19:51:39 +0100

    Setup: data blocks for ami and iam policy

---
+++ instances.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "al2023" {
+  most_recent = true // use an ami id for this i am using most recent = true as a prefernce
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
