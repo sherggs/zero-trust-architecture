efc4e42389576c0d9843f371294c3a5b341297b0
sherggs <sherggs@gmail.com>
2026-07-20 19:05:16 +0100

    Lambda: Resources for lambda

---
+++ lambda.tf
@@ -0,0 +1,29 @@
+# Lambda.
+# No PutItem, no UpdateItem, no DeleteItem, no wildcard. A compromised function cannot write, delete, or reach other tables.
+
+resource "aws_iam_role" "lambda" {
+  name               = "${var.project_name}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+}
+
+resource "aws_iam_role_policy" "lambda" {
+  name   = "${var.project_name}-lambda-policy"
+  role   = aws_iam_role.lambda.id
+  policy = data.aws_iam_policy_document.lambda_permissions.json
+}
+
+resource "aws_lambda_function" "read_inventory" {
+  function_name    = "${var.project_name}-read-inventory"
+  role             = aws_iam_role.lambda.arn
+  runtime          = "python3.12"
+  handler          = "handler.lambda_handler"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  timeout          = 10
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.zero_trust_table.name
+    }
+  }
+}
