998e97e429f5321c0da8202da74f7bca18702578
sherggs <77879271+sherggs@users.noreply.github.com>
2026-07-20 21:43:33 +0100

    vpc: cloudwatch and flow logs

--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,31 @@ resource "aws_vpc" "zero_trust" {
   }
 }
 
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/${var.project_name}-flow-logs"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name               = "${var.project_name}-vpc-flow-logs-role"
+  assume_role_policy = data.aws_iam_policy_document.vpc_flow_logs_assume.json
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs" {
+  name   = "${var.project_name}-vpc-flow-logs-policy"
+  role   = aws_iam_role.vpc_flow_logs.id
+  policy = data.aws_iam_policy_document.vpc_flow_logs_permissions.json
+}
+
+resource "aws_flow_log" "zero_trust" {
+  vpc_id               = aws_vpc.zero_trust.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_logs.arn
+}
+
 resource "aws_internet_gateway" "zero_trust" {
   vpc_id = aws_vpc.zero_trust.id
 
@@ -18,6 +43,7 @@ resource "aws_internet_gateway" "zero_trust" {
   }
 }
 
+#tfsec:ignore:aws-ec2-no-public-ip-subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.zero_trust.id
   cidr_block              = var.public_subnet_cidr
